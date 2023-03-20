d59ff77096876cb578934aacb02dfaa1d37fbba3
ashfak-rahad <ashfakur@gmail.com>
2023-03-20 20:15:50 +0600

    some new data load

--- js/countries.js
+++ js/countries.js
@@ -5,17 +5,53 @@ const loadCountries =() =>{
 }
 
 const displayCountries = countries =>{
-//    console.log(countries);
+   console.log(countries[1]);
    const allCountriesHTML = countries.map(country=>getCountryHTML(country));
 //    console.log(allCountriesHTML);
  const container = document.getElementById('countries');
- container.innerHTML =allCountriesHTML.join('');
+ container.innerHTML = allCountriesHTML.join('');
 }
-const getCountryHTML = country =>{
+// option 1 destructuring
+// const getCountryHTML = country =>{
+//   // option 1
+//   const {name,flags} = country;
+//     return`
+//       <div class="country">
+//         <h2>${name.common}</h2>
+//         <img src ="${flags.png}">
+
+//       </div>
+//     `
+// }
+
+// option 2 destructuring
+// const getCountryHTML = ({name,flags}) =>{
+//   
+//   const {name,flags} = country;
+//     return`
+//       <div class="country">
+//         <h2>${name.common}</h2>
+//         <img src ="${flags.png}">
+
+//       </div>
+//     `
+// }
+
+const getCountryHTML = ({name,flags,capital,area,continents,population,translations
+}) =>{
+  
     return`
       <div class="country">
-        <h2>${country.name.common}</h2>
-        <img src ="${country.flags.png}">
+        <h2>${name.common}</h2>
+        <h3>Capital:${capital}</h3>
+        <p>Area:${area}</p>
+        
+        <p>continent:${continents}</p>
+        <p>population:${population}</p>
+        <p>translations:${translations.ara.official}</p>
+
+
+        <img src ="${flags.png}">
 
       </div>
     `
